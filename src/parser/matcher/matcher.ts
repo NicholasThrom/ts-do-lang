d6fd6abc14a5c59750865cd12bf39a05e96976b1
NicholasThrom <narthrom@edu.uwaterloo.ca>
2018-03-02 00:21:45 -0500

    Added abstract Matcher base class.

---
+++ src/parser/matcher/matcher.ts
@@ -0,0 +1,56 @@
+
+/**
+ * A finite state machine used to match against arbitrary series of data.
+ *
+ * Similar to the build in `RegExp` type but
+ * - Matches things other than strings.
+ * - Can note different kinds of matches.
+ *
+ * Usage:
+ * ```
+ * matcher.steps([new Type('a'), new Type('a'), new Type('a')]).value)
+ * // Outputs some `Value`.
+ * ```
+ *
+ * @param Type The type of the input to match against.
+ * @param Value The type that indicates the kind of match at this matcher.
+ */
+abstract class Matcher<Type, Value> {
+
+    /**
+     * The kind of match at this matcher.
+     *
+     * If this matcher is not a match, null.
+     */
+    public abstract readonly value: Value | null;
+
+    /**
+     * Returns the next `Matcher`, or `null` if the match is over.
+     *
+     * @param input The step to the next `Matcher`
+     * @returns The next `Matcher`, or `null` if the match is over.
+     */
+    public abstract step(input: Type): this | null;
+
+    /**
+     * Returns a tuple containing the final `Matcher` and the index of the last
+     * matched entry.
+     *
+     * @param inputs The inputs over which to match.
+     */
+    public steps(inputs: Type[]): [this, number] {
+
+        let index = 0;
+        let matcher = this;
+
+        for (const input of inputs) {
+            const next = this.step(input);
+            if (!next) { break; }
+            matcher = next;
+            index += 1;
+        }
+
+        return [matcher, index];
+    }
+
+}
