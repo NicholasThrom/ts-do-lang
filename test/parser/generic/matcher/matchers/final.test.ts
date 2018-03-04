2eb9b67a174422811a4b8988402920135b19bc8c
NicholasThrom <narthrom@edu.uwaterloo.ca>
2018-03-04 11:11:49 -0500

    Tested matchers/final.ts.

---
+++ test/parser/generic/matcher/matchers/final.test.ts
@@ -0,0 +1,39 @@
+import { assert } from "chai";
+import "mocha";
+import * as sinon from "sinon";
+const sandbox = sinon.sandbox.create();
+
+// Subject
+import { FinalMatcher } from "parser/generic/matcher/matchers/final";
+
+describe("parser/generic/matcher/matchers/final", function () {
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it("should exist", function () {
+        assert(FinalMatcher);
+    });
+
+    describe(".doesMatch", function () {
+
+        it("should be true", function () {
+            const matcher = new FinalMatcher<number>();
+
+            assert.strictEqual(matcher.doesMatch, true);
+        });
+
+    });
+
+    describe(".step", function () {
+
+        it("should return null for all inputs.", function () {
+            const matcher = new FinalMatcher<any>();
+
+            assert.strictEqual(matcher.step("any input"), null);
+        });
+
+    });
+
+});
